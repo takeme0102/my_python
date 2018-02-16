0ee418e482ae867694eff2d743de47e5f5120f5d
takeme0102 <g031n120@s.iwate-pu.ac.jp>
2018-02-16 18:07:44 +0900

    Web test

---
+++ kakugen.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 16:57:18 2018
+
+@author: takumi
+"""
+
+print("Content-Type: text/html; charset=utf-8")
+
+print("")
+
+print("<html><head><meta charset='utf-8'><body>")
+print("聞くことに速く語ることに遅くあるべき")
+print("</body></html>")
